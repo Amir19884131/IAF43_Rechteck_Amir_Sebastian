2cafc827d1f73a98aac534992da3c9e41860c2e1
Sebastian Dyga <sebastian.d27@SCHULE.KOELN>
2026-05-12 12:07:02 +0200

    test

--- Rechteckprojekt/Rechteckprojekt/Form1.cs
+++ Rechteckprojekt/Rechteckprojekt/Form1.cs
@@ -6,5 +6,10 @@ namespace Rechteckprojekt
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.Write("Döner Bober");
+        }
     }
 }
