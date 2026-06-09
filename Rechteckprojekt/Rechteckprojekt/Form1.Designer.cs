ebf4fd7e281abbd72747ff2d5e371b44f3798f81
Amir19884131 <amirhossei.r4@gso.schule.koeln>
2026-06-09 12:24:09 +0200

    GUI Update

--- Rechteckprojekt/Rechteckprojekt/Form1.Designer.cs
+++ Rechteckprojekt/Rechteckprojekt/Form1.Designer.cs
@@ -20,12 +20,6 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             btnZoomOut = new Button();
@@ -34,7 +28,7 @@
             tbxBreite = new TextBox();
             tbxHoehe = new TextBox();
             tbxUmfang = new TextBox();
-            tbxDiagonale = new TextBox();
+            tbxDiagonaleCM = new TextBox();
             tbxFlaeche = new TextBox();
             lblBreite = new Label();
             lblHoehe = new Label();
@@ -43,11 +37,26 @@
             lblFlaeche = new Label();
             btnRechteck = new Button();
             pnlLinie = new Panel();
+            lblBreiteCM = new Label();
+            lblHoeheCM = new Label();
+            lblUmfangCM = new Label();
+            lblDiagonaleCM = new Label();
+            lblFleacheCM = new Label();
+            tbxDiagonaleZOLL = new TextBox();
+            lblDiagonaleZOLL = new Label();
+            cbxFarben = new ComboBox();
+            lblExtra = new Label();
+            lblFarben = new Label();
+            tbxTextinhalt = new TextBox();
+            lblTextinhalt = new Label();
+            tbxRand = new TextBox();
+            lblRandCM = new Label();
+            lblRand = new Label();
             SuspendLayout();
             // 
             // btnZoomOut
             // 
-            btnZoomOut.Location = new Point(168, 311);
+            btnZoomOut.Location = new Point(168, 340);
             btnZoomOut.Margin = new Padding(3, 2, 3, 2);
             btnZoomOut.Name = "btnZoomOut";
             btnZoomOut.Size = new Size(97, 41);
@@ -58,7 +67,7 @@
             // 
             // btnZoomIn
             // 
-            btnZoomIn.Location = new Point(58, 311);
+            btnZoomIn.Location = new Point(58, 340);
             btnZoomIn.Margin = new Padding(3, 2, 3, 2);
             btnZoomIn.Name = "btnZoomIn";
             btnZoomIn.Size = new Size(95, 41);
@@ -69,7 +78,7 @@
             // 
             // btnDrehen
             // 
-            btnDrehen.Location = new Point(58, 365);
+            btnDrehen.Location = new Point(58, 394);
             btnDrehen.Margin = new Padding(3, 2, 3, 2);
             btnDrehen.Name = "btnDrehen";
             btnDrehen.Size = new Size(207, 51);
@@ -82,15 +91,15 @@
             tbxBreite.Location = new Point(147, 48);
             tbxBreite.Margin = new Padding(3, 2, 3, 2);
             tbxBreite.Name = "tbxBreite";
-            tbxBreite.Size = new Size(110, 23);
+            tbxBreite.Size = new Size(88, 23);
             tbxBreite.TabIndex = 3;
             // 
             // tbxHoehe
             // 
-            tbxHoehe.Location = new Point(147, 80);
+            tbxHoehe.Location = new Point(147, 93);
             tbxHoehe.Margin = new Padding(3, 2, 3, 2);
             tbxHoehe.Name = "tbxHoehe";
-            tbxHoehe.Size = new Size(110, 23);
+            tbxHoehe.Size = new Size(88, 23);
             tbxHoehe.TabIndex = 4;
             // 
             // tbxUmfang
@@ -99,31 +108,31 @@
             tbxUmfang.Margin = new Padding(3, 2, 3, 2);
             tbxUmfang.Name = "tbxUmfang";
             tbxUmfang.ReadOnly = true;
-            tbxUmfang.Size = new Size(119, 23);
+            tbxUmfang.Size = new Size(88, 23);
             tbxUmfang.TabIndex = 5;
             // 
-            // tbxDiagonale
+            // tbxDiagonaleCM
             // 
-            tbxDiagonale.Location = new Point(147, 214);
-            tbxDiagonale.Margin = new Padding(3, 2, 3, 2);
-            tbxDiagonale.Name = "tbxDiagonale";
-            tbxDiagonale.ReadOnly = true;
-            tbxDiagonale.Size = new Size(119, 23);
-            tbxDiagonale.TabIndex = 6;
+            tbxDiagonaleCM.Location = new Point(147, 221);
+            tbxDiagonaleCM.Margin = new Padding(3, 2, 3, 2);
+            tbxDiagonaleCM.Name = "tbxDiagonaleCM";
+            tbxDiagonaleCM.ReadOnly = true;
+            tbxDiagonaleCM.Size = new Size(88, 23);
+            tbxDiagonaleCM.TabIndex = 6;
             // 
             // tbxFlaeche
             // 
-            tbxFlaeche.Location = new Point(147, 250);
+            tbxFlaeche.Location = new Point(147, 301);
             tbxFlaeche.Margin = new Padding(3, 2, 3, 2);
             tbxFlaeche.Name = "tbxFlaeche";
             tbxFlaeche.ReadOnly = true;
-            tbxFlaeche.Size = new Size(119, 23);
+            tbxFlaeche.Size = new Size(88, 23);
             tbxFlaeche.TabIndex = 7;
             // 
             // lblBreite
             // 
             lblBreite.AutoSize = true;
-            lblBreite.Location = new Point(58, 53);
+            lblBreite.Location = new Point(58, 56);
             lblBreite.Name = "lblBreite";
             lblBreite.Size = new Size(43, 15);
             lblBreite.TabIndex = 8;
@@ -132,7 +141,7 @@
             // lblHoehe
             // 
             lblHoehe.AutoSize = true;
-            lblHoehe.Location = new Point(58, 86);
+            lblHoehe.Location = new Point(58, 101);
             lblHoehe.Name = "lblHoehe";
             lblHoehe.Size = new Size(42, 15);
             lblHoehe.TabIndex = 9;
@@ -141,7 +150,7 @@
             // lblUmfang
             // 
             lblUmfang.AutoSize = true;
-            lblUmfang.Location = new Point(58, 182);
+            lblUmfang.Location = new Point(58, 185);
             lblUmfang.Name = "lblUmfang";
             lblUmfang.Size = new Size(56, 15);
             lblUmfang.TabIndex = 10;
@@ -150,7 +159,7 @@
             // lblDiagonale
             // 
             lblDiagonale.AutoSize = true;
-            lblDiagonale.Location = new Point(58, 219);
+            lblDiagonale.Location = new Point(58, 229);
             lblDiagonale.Name = "lblDiagonale";
             lblDiagonale.Size = new Size(66, 15);
             lblDiagonale.TabIndex = 11;
@@ -159,7 +168,7 @@
             // lblFlaeche
             // 
             lblFlaeche.AutoSize = true;
-            lblFlaeche.Location = new Point(58, 256);
+            lblFlaeche.Location = new Point(58, 309);
             lblFlaeche.Name = "lblFlaeche";
             lblFlaeche.Size = new Size(47, 15);
             lblFlaeche.TabIndex = 12;
@@ -168,12 +177,12 @@
             // btnRechteck
             // 
             btnRechteck.Enabled = false;
-            btnRechteck.Location = new Point(375, 76);
+            btnRechteck.Location = new Point(806, 340);
             btnRechteck.Margin = new Padding(3, 2, 3, 2);
             btnRechteck.Name = "btnRechteck";
-            btnRechteck.Size = new Size(402, 277);
+            btnRechteck.Size = new Size(508, 276);
             btnRechteck.TabIndex = 13;
-            btnRechteck.Text = "Ich bin Rechteck (kein button)";
+            btnRechteck.Text = "Hier kann Ihr Text stehen";
             btnRechteck.UseVisualStyleBackColor = true;
             // 
             // pnlLinie
@@ -182,14 +191,160 @@
             pnlLinie.Location = new Point(330, -1);
             pnlLinie.Margin = new Padding(3, 2, 3, 2);
             pnlLinie.Name = "pnlLinie";
-            pnlLinie.Size = new Size(4, 456);
+            pnlLinie.Size = new Size(4, 1000);
             pnlLinie.TabIndex = 14;
+            // 
+            // lblBreiteCM
+            // 
+            lblBreiteCM.AutoSize = true;
+            lblBreiteCM.Location = new Point(241, 56);
+            lblBreiteCM.Name = "lblBreiteCM";
+            lblBreiteCM.Size = new Size(24, 15);
+            lblBreiteCM.TabIndex = 15;
+            lblBreiteCM.Text = "cm";
+            // 
+            // lblHoeheCM
+            // 
+            lblHoeheCM.AutoSize = true;
+            lblHoeheCM.Location = new Point(241, 101);
+            lblHoeheCM.Name = "lblHoeheCM";
+            lblHoeheCM.Size = new Size(24, 15);
+            lblHoeheCM.TabIndex = 16;
+            lblHoeheCM.Text = "cm";
+            // 
+            // lblUmfangCM
+            // 
+            lblUmfangCM.AutoSize = true;
+            lblUmfangCM.Location = new Point(241, 185);
+            lblUmfangCM.Name = "lblUmfangCM";
+            lblUmfangCM.Size = new Size(24, 15);
+            lblUmfangCM.TabIndex = 17;
+            lblUmfangCM.Text = "cm";
+            // 
+            // lblDiagonaleCM
+            // 
+            lblDiagonaleCM.AutoSize = true;
+            lblDiagonaleCM.Location = new Point(241, 229);
+            lblDiagonaleCM.Name = "lblDiagonaleCM";
+            lblDiagonaleCM.Size = new Size(24, 15);
+            lblDiagonaleCM.TabIndex = 18;
+            lblDiagonaleCM.Text = "cm";
+            // 
+            // lblFleacheCM
+            // 
+            lblFleacheCM.AutoSize = true;
+            lblFleacheCM.Location = new Point(241, 309);
+            lblFleacheCM.Name = "lblFleacheCM";
+            lblFleacheCM.Size = new Size(24, 15);
+            lblFleacheCM.TabIndex = 19;
+            lblFleacheCM.Text = "cm";
+            // 
+            // tbxDiagonaleZOLL
+            // 
+            tbxDiagonaleZOLL.Location = new Point(147, 261);
+            tbxDiagonaleZOLL.Name = "tbxDiagonaleZOLL";
+            tbxDiagonaleZOLL.ReadOnly = true;
+            tbxDiagonaleZOLL.Size = new Size(88, 23);
+            tbxDiagonaleZOLL.TabIndex = 20;
+            // 
+            // lblDiagonaleZOLL
+            // 
+            lblDiagonaleZOLL.AutoSize = true;
+            lblDiagonaleZOLL.Location = new Point(241, 269);
+            lblDiagonaleZOLL.Name = "lblDiagonaleZOLL";
+            lblDiagonaleZOLL.Size = new Size(25, 15);
+            lblDiagonaleZOLL.TabIndex = 21;
+            lblDiagonaleZOLL.Text = "zoll";
+            // 
+            // cbxFarben
+            // 
+            cbxFarben.FormattingEnabled = true;
+            cbxFarben.Items.AddRange(new object[] { "Weiß", "Gelb", "Rot", "Blau", "Orange", "Lila", "Schwarz", "Grau", "Grün" });
+            cbxFarben.Location = new Point(127, 558);
+            cbxFarben.Name = "cbxFarben";
+            cbxFarben.Size = new Size(138, 23);
+            cbxFarben.TabIndex = 22;
+            // 
+            // lblExtra
+            // 
+            lblExtra.AutoSize = true;
+            lblExtra.Location = new Point(58, 525);
+            lblExtra.Name = "lblExtra";
+            lblExtra.Size = new Size(37, 15);
+            lblExtra.TabIndex = 23;
+            lblExtra.Text = "Extras";
+            // 
+            // lblFarben
+            // 
+            lblFarben.AutoSize = true;
+            lblFarben.Location = new Point(58, 566);
+            lblFarben.Name = "lblFarben";
+            lblFarben.Size = new Size(49, 15);
+            lblFarben.TabIndex = 24;
+            lblFarben.Text = "Farben :";
+            // 
+            // tbxTextinhalt
+            // 
+            tbxTextinhalt.Location = new Point(127, 601);
+            tbxTextinhalt.Multiline = true;
+            tbxTextinhalt.Name = "tbxTextinhalt";
+            tbxTextinhalt.Size = new Size(139, 80);
+            tbxTextinhalt.TabIndex = 25;
+            // 
+            // lblTextinhalt
+            // 
+            lblTextinhalt.AutoSize = true;
+            lblTextinhalt.Location = new Point(58, 601);
+            lblTextinhalt.Name = "lblTextinhalt";
+            lblTextinhalt.Size = new Size(34, 15);
+            lblTextinhalt.TabIndex = 26;
+            lblTextinhalt.Text = "Text :";
+            // 
+            // tbxRand
+            // 
+            tbxRand.Location = new Point(127, 701);
+            tbxRand.Name = "tbxRand";
+            tbxRand.Size = new Size(108, 23);
+            tbxRand.TabIndex = 27;
+            // 
+            // lblRandCM
+            // 
+            lblRandCM.AutoSize = true;
+            lblRandCM.Location = new Point(241, 709);
+            lblRandCM.Name = "lblRandCM";
+            lblRandCM.Size = new Size(24, 15);
+            lblRandCM.TabIndex = 28;
+            lblRandCM.Text = "cm";
+            // 
+            // lblRand
+            // 
+            lblRand.AutoSize = true;
+            lblRand.Location = new Point(58, 709);
+            lblRand.Name = "lblRand";
+            lblRand.Size = new Size(40, 15);
+            lblRand.TabIndex = 29;
+            lblRand.Text = "Rand :";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1774, 892);
+            Controls.Add(lblRand);
+            Controls.Add(lblRandCM);
+            Controls.Add(tbxRand);
+            Controls.Add(lblTextinhalt);
+            Controls.Add(tbxTextinhalt);
+            Controls.Add(lblFarben);
+            Controls.Add(lblExtra);
+            Controls.Add(cbxFarben);
+            Controls.Add(lblDiagonaleZOLL);
+            Controls.Add(tbxDiagonaleZOLL);
+            Controls.Add(lblFleacheCM);
+            Controls.Add(lblDiagonaleCM);
+            Controls.Add(lblUmfangCM);
+            Controls.Add(lblHoeheCM);
+            Controls.Add(lblBreiteCM);
             Controls.Add(pnlLinie);
             Controls.Add(btnRechteck);
             Controls.Add(lblFlaeche);
@@ -198,7 +353,7 @@
             Controls.Add(lblHoehe);
             Controls.Add(lblBreite);
             Controls.Add(tbxFlaeche);
-            Controls.Add(tbxDiagonale);
+            Controls.Add(tbxDiagonaleCM);
             Controls.Add(tbxUmfang);
             Controls.Add(tbxHoehe);
             Controls.Add(tbxBreite);
@@ -212,7 +367,6 @@
             PerformLayout();
         }
 
-        #endregion
 
         private Button btnZoomOut;
         private Button btnZoomIn;
@@ -220,7 +374,7 @@
         private TextBox tbxBreite;
         private TextBox tbxHoehe;
         private TextBox tbxUmfang;
-        private TextBox tbxDiagonale;
+        private TextBox tbxDiagonaleCM;
         private TextBox tbxFlaeche;
         private Label lblBreite;
         private Label lblHoehe;
@@ -229,5 +383,20 @@
         private Label lblFlaeche;
         private Button btnRechteck;
         private Panel pnlLinie;
+        private Label lblBreiteCM;
+        private Label lblHoeheCM;
+        private Label lblUmfangCM;
+        private Label lblDiagonaleCM;
+        private Label lblFleacheCM;
+        private TextBox tbxDiagonaleZOLL;
+        private Label lblDiagonaleZOLL;
+        private ComboBox cbxFarben;
+        private Label lblExtra;
+        private Label lblFarben;
+        private TextBox tbxTextinhalt;
+        private Label lblTextinhalt;
+        private TextBox tbxRand;
+        private Label lblRandCM;
+        private Label lblRand;
     }
 }
